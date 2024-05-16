c37158037b0b7f912cd6691e88b0d42fc9521800
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2024-05-16 08:35:35 -0600

    test: allow all cross origins

--- src/main/java/com/shuttleconsulting/fileuploadedapi/controller/FileController.java
+++ src/main/java/com/shuttleconsulting/fileuploadedapi/controller/FileController.java
@@ -14,6 +14,7 @@ import java.nio.file.Path;
 
 import static com.shuttleconsulting.fileuploadedapi.dto.response.GenericResponse.GenerateHttpResponse;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/file")
 public class FileController {
@@ -26,7 +27,7 @@ public class FileController {
   }
 
   @PostMapping()
-  @CrossOrigin(origins = {"http://localhost:*", "https://localhost:*"})
+  //@CrossOrigin(origins = {"http://localhost:*", "https://localhost:*"})
   public ResponseEntity<GenericResponse<FileUploadResponseDto>> uploadFile(@RequestParam("file") MultipartFile file) {
     return GenerateHttpResponse(service.store(file));
   }
